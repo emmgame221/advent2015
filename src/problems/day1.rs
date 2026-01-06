d862c9a947cc90e99c17e5bf0e18afb292ea4ea2
Eric McHugh <emmgame221@gmail.com>
2026-01-06 02:53:25 -0500

    Day 1 done

--- src/problems/day1.rs
+++ src/problems/day1.rs
@@ -1,3 +1,38 @@
+use std::{fs::File, io::Read};
+
 pub fn print_solution() {
-    
+    let mut file = File::open("./inputs/day1.txt").unwrap();
+    let mut input: String = String::with_capacity(8000);
+    file.read_to_string(&mut input).unwrap();
+    let p1 = part_one(&input);
+    let p2 = part_two(&input);
+    println!("Day 1 Part 1: {}", p1);
+    println!("Day 1 Part 2: {}", p2);
+}
+
+fn part_one(input: &str) -> i64 {
+    let mut floor = 0;
+    for c in input.chars() {
+        match c {
+            '(' => floor += 1,
+            ')' => floor -= 1,
+            _ => {}
+        }
+    }
+    floor
+}
+
+fn part_two(input: &str) -> usize {
+    let mut floor = 0;
+    for (i, c) in input.chars().enumerate() {
+        match c {
+            '(' => floor += 1,
+            ')' => floor -= 1,
+            _ => {}
+        }
+        if floor == -1 {
+            return i + 1;
+        }
+    }
+    0
 }
