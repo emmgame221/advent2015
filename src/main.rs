398ea6e89e1b90b3031ef9d32a3f6c9b02bfe0ae
Eric McHugh <emmgame221@gmail.com>
2026-01-06 02:38:49 -0500

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,34 @@
+use clap::{command, Arg};
+
+mod problems;
+
+use crate::problems::*;
+
+fn main() {
+    let matches = command!() 
+        .arg(Arg::new("problems").short('p').long("problems"))
+        .get_matches();
+    let args = matches
+        .get_many::<String>("problems")
+        .unwrap_or_default()
+        .map(|v| v.as_str())
+        .collect::<Vec<_>>();
+    if args.contains(&"all") {
+        run_all();
+    } else {
+        for arg in args {
+            match arg {
+                "1" => {
+                    day1::print_solution();
+                }
+                _ => {
+                    println!("Unknown Problem")
+                }
+            }
+        }
+    }
+}
+
+fn run_all() {
+    day1::print_solution();
+}
